f086e35c7e41f052391e38d4f34f06d1f64e8cd3
Seokju <iamseokjuhong@gmail.com>
2024-08-23 12:33:39 +0900

    OCP: Open-Closed Principle

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleOutputHandler.java
@@ -3,8 +3,11 @@ package cleancode.minesweeper.tobe.io;
 import cleancode.minesweeper.tobe.GameBoard;
 import cleancode.minesweeper.tobe.GameException;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class ConsoleOutputHandler {
-    
+
     public void showGameStartComments() {
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         System.out.println("지뢰찾기 게임 시작!");
@@ -12,15 +15,25 @@ public class ConsoleOutputHandler {
     }
 
     public void showBoard(GameBoard board) {
-        System.out.println("   a b c d e f g h i j");
+        String alphabets = generateColAlphabets(board);
+
+        System.out.println("    " + alphabets);
         for (int row = 0; row < board.getRowSize(); row++) {
-            System.out.printf("%d  ", row + 1);
+            System.out.printf("%2d  ", row + 1);
             for (int col = 0; col < board.getColSize(); col++) {
                 System.out.print(board.getSign(row, col) + " ");
             }
             System.out.println();
         }
         System.out.println();
+    }
+
+    private String generateColAlphabets(GameBoard board) {
+        List<String> alphabets = IntStream.range(0, board.getColSize())
+                .mapToObj(index -> (char) ('a' + index))
+                .map(Object::toString)
+                .toList();
+        return String.join(" ", alphabets);
     }
 
     public void printGameWinningComment() {
@@ -32,11 +45,11 @@ public class ConsoleOutputHandler {
     }
 
     public void printCommentForSelectingCell() {
-        System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
+        System.out.println("선택할 좌표를 입력하세요. (예: a1)");
     }
 
     public void printCommentForUserAction() {
-        System.out.println("선택할 좌표를 입력하세요. (예: a1)");
+        System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
     }
 
     public void printExceptionMessage(GameException e) {
@@ -46,4 +59,5 @@ public class ConsoleOutputHandler {
     public void printSimpleMessage(String message) {
         System.out.println(message);
     }
+
 }
