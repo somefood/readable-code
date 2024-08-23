aab5a4d4d0c771a27eecc944862b55ab88890079
Seokju <iamseokjuhong@gmail.com>
2024-08-23 13:02:39 +0900

    LSP: Liskov Substitution Principle

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -5,7 +5,7 @@ import cleancode.minesweeper.tobe.gamelevel.*;
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameLevel gameLevel = new VeryBeginner();
+        GameLevel gameLevel = new Middle();
         
         Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
