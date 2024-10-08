db3f5ba5e66e7b7e341934b56361f0e85025a73d
Seokju Hong <iamseokjuhong@gmail.com>
2024-10-09 00:30:06 +0900

    Readable Code(Restudy) - 리팩토링 연습 - 구체타입을 생성자에서 주입받게 수정

--- src/main/java/cleancode/studycafe/seokju_tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/seokju_tobe/StudyCafeApplication.java
@@ -1,9 +1,15 @@
 package cleancode.studycafe.seokju_tobe;
 
+import cleancode.studycafe.seokju_tobe.io.ConsoleInputHandler;
+import cleancode.studycafe.seokju_tobe.io.ConsoleOutputHandler;
+
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(
+            new ConsoleInputHandler(),
+            new ConsoleOutputHandler()
+        );
         studyCafePassMachine.run();
     }
 
