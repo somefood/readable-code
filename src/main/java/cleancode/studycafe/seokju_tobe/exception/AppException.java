741d794cff07c6da1371f803fc741279a3c2b716
Seokju Hong <iamseokjuhong@gmail.com>
2024-10-09 00:16:23 +0900

    Readable Code(Restudy) - 리팩토링 연습 시작

---
+++ src/main/java/cleancode/studycafe/seokju_tobe/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.seokju_tobe.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
