21134bff37371a08c4090a43b1563049c9382a02
Seokju <iamseokjuhong@gmail.com>
2024-10-10 12:07:49 +0900

    Readable Code(Restudy) - 리팩토링 연습(1) 추상화 레벨

---
+++ src/main/java/cleancode/studycafe/tobe2/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package cleancode.studycafe.tobe2;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
